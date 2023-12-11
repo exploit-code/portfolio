b97b4d4b886ca0b29b1c63644d141ea136426808
npc_unit <o_xx@list.ru>
2023-12-12 02:17:05 +0300

    remove project

--- src/utils/projects-data.js
+++ src/utils/projects-data.js
@@ -11,31 +11,52 @@ export const projects = [
   {
     link: "https://fake-shop-exploit-code.vercel.app/",
     title: "Fake-Shop",
-    technologies: [<ReactIcon key="ReactIcon" />, <ReduxIcon key="ReduxIcon" />, <SassIcon key="SassIcon" />],
+    technologies: [
+      <ReactIcon key="ReactIcon" />,
+      <ReduxIcon key="ReduxIcon" />,
+      <SassIcon key="SassIcon" />,
+    ],
     uuid: uuidv4(),
   },
   {
     link: "https://github-search-ruddy.vercel.app/",
     title: "GitHub Users Search",
-    technologies: [<ReactIcon key="ReactIcon" />, <TSIcon key="TSIcon" />, <ReduxIcon key="ReduxIcon" />, <SassIcon key="SassIcon" />],
+    technologies: [
+      <ReactIcon key="ReactIcon" />,
+      <TSIcon key="TSIcon" />,
+      <ReduxIcon key="ReduxIcon" />,
+      <SassIcon key="SassIcon" />,
+    ],
     uuid: uuidv4(),
   },
   {
     link: "https://drweb.africa/landing/partner_demo",
     title: "Become a partner of the Doctor Web brand",
-    technologies: [<HtmlIcon key="HtmlIcon" />, <SassIcon key="SassIcon" />, <AlpineIcon key="AlpineIcon" />],
+    technologies: [
+      <HtmlIcon key="HtmlIcon" />,
+      <SassIcon key="SassIcon" />,
+      <AlpineIcon key="AlpineIcon" />,
+    ],
     uuid: uuidv4(),
   },
   {
     link: "https://www.drweb.cn/landing/promo_drweb",
     title: "Dr.Web Security Space",
-    technologies: [<HtmlIcon key="HtmlIcon" />, <SassIcon key="SassIcon" />, <JsIcon key="JsIcon" />],
+    technologies: [
+      <HtmlIcon key="HtmlIcon" />,
+      <SassIcon key="SassIcon" />,
+      <JsIcon key="JsIcon" />,
+    ],
     uuid: uuidv4(),
   },
   {
     link: "https://drweb.africa/landing/products_home",
     title: "Protect your world",
-    technologies: [<HtmlIcon key="HtmlIcon" />, <SassIcon key="SassIcon" />, <AlpineIcon key="AlpineIcon" />],
+    technologies: [
+      <HtmlIcon key="HtmlIcon" />,
+      <SassIcon key="SassIcon" />,
+      <AlpineIcon key="AlpineIcon" />,
+    ],
     uuid: uuidv4(),
   },
   {
@@ -47,19 +68,23 @@ export const projects = [
   {
     link: "https://products.drweb.ru/biz/v4",
     title: "License Builder",
-    technologies: [<HtmlIcon key="HtmlIcon" />, <SassIcon key="SassIcon" />, <JsIcon key="JsIcon" />, <AlpineIcon key="AlpineIcon" />],
+    technologies: [
+      <HtmlIcon key="HtmlIcon" />,
+      <SassIcon key="SassIcon" />,
+      <JsIcon key="JsIcon" />,
+      <AlpineIcon key="AlpineIcon" />,
+    ],
     uuid: uuidv4(),
   },
   {
     link: "https://www.freeantivirusdownloads.com",
     title: "Free anti-virus Dr.Web",
-    technologies: [<HtmlIcon key="HtmlIcon" />, <SassIcon key="SassIcon" />, <JsIcon key="JsIcon" />, <AlpineIcon key="AlpineIcon" />],
-    uuid: uuidv4(),
-  },
-  {
-    link: "https://company.drweb.ru/press/presskit",
-    title: "Press kit Dr.Web",
-    technologies: [<HtmlIcon key="HtmlIcon" />, <SassIcon key="SassIcon" />, <JsIcon key="JsIcon" />, <AlpineIcon key="AlpineIcon" />],
+    technologies: [
+      <HtmlIcon key="HtmlIcon" />,
+      <SassIcon key="SassIcon" />,
+      <JsIcon key="JsIcon" />,
+      <AlpineIcon key="AlpineIcon" />,
+    ],
     uuid: uuidv4(),
   },
   {
